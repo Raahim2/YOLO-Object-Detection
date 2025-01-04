84d73c1f22eb2fa18e5aa5f4e5dee39ac47e22f0
Raahim <127599591+Raahim2@users.noreply.github.com>
2025-01-04 21:11:36 +0530

    Update index.py

--- index.py
+++ index.py
@@ -2,14 +2,14 @@ from flask import Flask , render_template , request , Response
 # from PIL import Image
 import io
 import numpy as np
-from ultralytics import YOLO
+# from ultralytics import YOLO
 # import cv2
 # import cvzone
 import math
 import base64
 
 # model = YOLO('yolov8n')
-model = "Yolo"
+model = "YOLO('yolov8n')"
 
 # CLASSES = model.names
 CLR= [
